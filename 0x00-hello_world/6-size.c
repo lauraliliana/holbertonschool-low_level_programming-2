4f06e41b9cca52cdf4746758344588842e15f697
root <root@LAPTOP-DD7UF48T.localdomain>
2020-09-16 16:56:00 -0500

    Add 6th

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ char chartype;
 long int longint;
 long long int longlongint;
 float floattype;
-printf("Size of a char: %ld byte(s)\n", sizeof(inttype));
 printf("Size of a char: %ld byte(s)\n", sizeof(chartype));
-printf("Size of a char: %ld byte(s)\n", sizeof(longint));
-printf("Size of a char: %ld byte(s)\n", sizeof(longlongint));
-printf("Size of a char: %ld byte(s)\n", sizeof(floattype));
+printf("Size of an int: %ld byte(s)\n", sizeof(inttype));
+printf("Size of a long int: %ld byte(s)\n", sizeof(longint));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongint));
+printf("Size of a float: %ld byte(s)\n", sizeof(floattype));
 return (0);
 }
