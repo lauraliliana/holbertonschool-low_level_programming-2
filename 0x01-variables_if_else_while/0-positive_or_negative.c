34779b4564fd4cc0c5341a812139b5215b1e3d79
root <root@LAPTOP-DD7UF48T.localdomain>
2020-09-17 12:20:14 -0500

    corrected 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,8 +13,8 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive\n", n);
 if (n == 0)
-printf("%d is positive\n", n);
+printf("%d is zero\n", n);
 if (n < 0)
-printf("%d is positive\n", n);
+printf("%d is negative\n", n);
 return (0);
 }
