ca984adbb4232f0ab06ad256d8a078ce34ba0bb5
root <root@LAPTOP-DD7UF48T.localdomain>
2020-09-22 13:36:27 -0500

    add exercise 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *main - Prints the method
+ *@n: int to operate
+ *@m: int to operate
+ *Return: Always 0
+ */
+int add(int n, int m)
+{
+  int s;
+  s = n + m;
+  return (s);
+  
+}
