3072ecb7e6b028d441fad6a6b42d32a75c66f100
root <root@LAPTOP-DD7UF48T.localdomain>
2020-09-22 13:43:05 -0500

    Corrected 5 and 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,19 @@
 #include "holberton.h"
 /**
  *_abs - Prints the method
+ *@r: Variable to operate
  *Return: Always 0
  */
 int _abs(int r)
 {
-  int s;
-  if (r < 0)
-    {
-      s = r * (-1);
-    }
-  else
-    {
-    s = r;
-    }
-    return (s);  
+int s;
+if (r < 0)
+{
+s = r * (-1);
+}
+else
+{
+s = r;
+}
+return (s);
 }
