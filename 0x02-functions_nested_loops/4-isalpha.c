cdf5adee149150f5cdcf54dc48b3002d14fb9b5f
root <root@LAPTOP-DD7UF48T.localdomain>
2020-09-22 13:26:48 -0500

    Corrections

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
  *_isalpha - Prints the method
+ *@c: Opertaive int
  *Return: Always 0
  */
 int _isalpha(int c)
 {
-  if (c > 'a' && c < 'z')
-    return (1);
-  else
-    return (0);  
+if (c > 'A' && c < 'z')
+return (1);
+else
+return (0);  
 }
