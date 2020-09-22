178a11cce3be1f9116d603345eb40a2cc1c46efe
root <root@LAPTOP-DD7UF48T.localdomain>
2020-09-22 13:22:33 -0500

    Betty correction 3th

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,12 @@
 /**
  *_islower - Prints the method
  *Return: Always 0
+ *@c: operative int
  */
 int _islower(int c)
 {
-  if (c > 'a' && c < 'z')
-      return (1);
-  else
-    return (0);
+if (c > 'a' && c < 'z')
+return (1);
+else
+return (0);
 }
