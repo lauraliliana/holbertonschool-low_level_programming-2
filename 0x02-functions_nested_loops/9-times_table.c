8e7e683f4ad8501a123d2da1316e1ba3aed552c8
root <root@LAPTOP-DD7UF48T.localdomain>
2020-09-22 16:58:34 -0500

    Betty 9th

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,33 +5,32 @@
  */
 void times_table(void)
 {
-  int a, b, p;
-  for (a = 0; a <= 9; a++)
-    {
-    for (b = 0; b <= 9; b++)
-      {
-      p = a * b;
-      if (p > 9)
-	{
-	  if (b != 0)
-	    {
-	      _putchar(44);
-	      _putchar(32);
-	    }
-	  _putchar(p / 10 + '0');
-	  _putchar(p % 10 + '0');
-	}
-      if (p < 10)
-	{
-	  if (b != 0)
-	    {
-	      _putchar(44);
-	      _putchar(32);
-	      _putchar(32);
-	    }
-	  _putchar(p % 10 + '0');
-	}
-      }
-    _putchar(10);
-    }
+int a, b, p;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+p = a * b;
+if (p > 9)
+}
+if (b != 0)
+{
+_putchar(44);
+_putchar(32);
+}
+_putchar(p / 10 + '0');
+_putchar(p % 10 + '0');
+}
+if (p < 10)
+{
+if (b != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(p % 10 + '0');
+}
+_putchar(10);
+}
 }
