432d4193b70d9b2dbc08e015d647b04f51bf10b7
root <root@LAPTOP-DD7UF48T.localdomain>
2020-09-29 13:02:00 -0500

    Add 2nd

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
 /**
- *swap_int - Swaps 2 ints
+ *_strlen - Swaps 2 ints
  *@s: String to count
  *Return: Always 0
  */
 int _strlen(char *s)
 {
-  int l;
-  for (l = 0; s[l] != '\0'; ++l){}
-  return (l);
+int l;
+for (l = 0; s[l] != '\0'; ++l)
+{
+}
+return (l);
 }
