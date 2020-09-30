b6375c07a795caa6f3f3e0e780576d2423a9bf18
root <root@LAPTOP-DD7UF48T.localdomain>
2020-09-29 22:59:47 -0500

    Add 9 w betty

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,14 +7,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-  int i = 0, j;
-  while (src[i] != '\0')
-    {
-      i++;
-    }
-  for (j = 0; j <= i; j++)
-    {
-      dest[j] = *(src + j);
-    }
-  return (dest);
+int i = 0, j;
+while (src[i] != '\0')
+{
+i++;
+}
+for (j = 0; j <= i; j++)
+{
+dest[j] = *(src + j);
+}
+return (dest);
 }
