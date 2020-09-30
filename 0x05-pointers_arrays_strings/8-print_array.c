b52bb63307ad4a9ac6c89f97f9af452127b3d742
root <root@LAPTOP-DD7UF48T.localdomain>
2020-09-29 22:47:24 -0500

    Add 8 w betty

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,12 +8,17 @@
  */
 void print_array(int *a, int n)
 {
-  int i;
-  for (i = 0; i < n; i++)
-    {
-      if (i != n - 1)
-	printf("%i, ", a[i]);
-      else
-	printf("%i\n", a[i]);
+int i;
+if (n <= 0)
+{
+printf("\n");
+return;
+}
+for (i = 0; i < n; i++)
+{
+if (i != n - 1)
+printf("%i, ", a[i]);
+else
+printf("%i\n", a[i]);
 }
 }
