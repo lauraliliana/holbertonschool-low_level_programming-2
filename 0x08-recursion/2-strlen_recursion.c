8f2badf7385ee98be2aab954f545c359eda45d2f
root <root@LAPTOP-DD7UF48T.localdomain>
2020-10-08 00:03:48 -0500

    add 2nd w betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,9 +6,9 @@
  */
 int _strlen_recursion(char *s)
 {
-  if (*s)
-    {
-      return (_strlen_recursion(++s) + 1);
-     }
-  return (0);
+if (*s)
+{
+return (_strlen_recursion(++s) + 1);
+}
+return (0);
 }
