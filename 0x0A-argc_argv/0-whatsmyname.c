c4471b8d9ef896a84cd250186f66a058aa1479c1
root <root@LAPTOP-DD7UF48T.localdomain>
2020-10-11 22:50:40 -0500

    Check update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *_memcpy - copies string length
+ *@dest: string to change
+ *@src: string to add
+ *@n: number of items in the secont string
+ *Return: Always 0
+ */
+int main (int argc, char* argv[])
+{
+(void) argc;
+printf("%s", *argv);
+return (0);
+}
