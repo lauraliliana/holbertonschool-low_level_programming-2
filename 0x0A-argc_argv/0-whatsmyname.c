db2dacaf3ad7904f9a5052c7f4683bd4491a4796
root <root@LAPTOP-DD7UF48T.localdomain>
2020-10-11 22:56:54 -0500

    Add corrections to main program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,9 @@
  *@argv: Array with the argument's names
  *Return: Always 0
  */
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-(void) argc;
-printf("%s", *argv);
+(void)argc;
+printf("%s\n", *argv);
 return (0);
 }
