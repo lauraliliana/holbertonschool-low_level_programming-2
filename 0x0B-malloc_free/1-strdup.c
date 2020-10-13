f463241f1aa47597b06ac344d42a3c0caca0572e
root <root@LAPTOP-DD7UF48T.localdomain>
2020-10-13 18:44:35 -0500

    Add 1 w betty

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *_strdup -Copies a string to another memory location
+ *@str: Original string
+ *Return: Always 0
+ */
+char *_strdup(char *str)
+{
+int i, j = 0;
+char *s;
+if (str == NULL)
+return (NULL);
+while (str[j] != '\0')
+j++;
+s = malloc(sizeof(char) * (j + 1));
+for (i = 0; i < j; i++)
+{
+s[i] = str[i];
+}
+return (s);
+}
