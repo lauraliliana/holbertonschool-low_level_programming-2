721433abdc7decd8b4e3fccec38fda43ab1d1336
root <root@LAPTOP-DD7UF48T.localdomain>
2020-10-13 16:14:48 -0500

    Add 0 + correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,13 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *s;
-if (size <= 0 || size == 0)
+ if (size <= 0)
 {
 return ('\0');
 }
 s = malloc(size * sizeof(char));
+if (s == NULL)
+return ('\0');
 for (i = 0; i < size; i++)
 {
 *(s + i) = c;
