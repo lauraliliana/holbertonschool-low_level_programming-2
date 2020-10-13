b134da077fd8027e3d289429976c15a48448460f
root <root@LAPTOP-DD7UF48T.localdomain>
2020-10-13 16:08:52 -0500

    Add 0 w Betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,19 +8,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
-  unsigned int i;
-  char *s;
-  if (size <= 0)
-    {
-    return (0);
-    }
-  s = malloc(size * sizeof(char));
-  if (s == 0)
-    return (0);
-  for (i = 0; i < size; i++)
-    {
-      *(s + 1) = c;
-    }
-  *(s + i) = '\0';
-    return (s);    
+unsigned int i;
+char *s;
+if (size <= 0)
+{
+return (0);
+}
+s = malloc(size * sizeof(char));
+for (i = 0; i < size; i++)
+{
+*(s + i) = c;
+}
+*(s + i) = '\0';
+return (s);
 }
