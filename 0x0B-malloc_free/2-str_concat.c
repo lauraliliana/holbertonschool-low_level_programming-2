7e48af13b9a3b23b4c8290b63cdee247fbd1631f
root <root@LAPTOP-DD7UF48T.localdomain>
2020-10-13 22:17:03 -0500

    Add 2nd with conditionals

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,6 +10,10 @@ char *str_concat(char *s1, char *s2)
 {
 int i = 0, j = 0, c;
 char *con;
+if (s1 == NULL)
+s1 = ("");
+if (s2 == NULL)
+s2 = ("");
 while (s1[i] != '\0')
 i++;
 while (s2[j] != '\0')
