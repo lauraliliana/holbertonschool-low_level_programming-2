39628ce457df574d71a09422486abd13f4ed3a98
root <root@LAPTOP-DD7UF48T.localdomain>
2020-10-19 23:53:40 -0500

    Add 4th

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,8 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
-
-
 /**
  * _strdup - returns a pointer to a newly allocated space in memory, which
  * contains a copy of the string given as a parameter.
@@ -12,28 +10,22 @@
  */
 char *_strdup(char *str)
 {
-  int l, i;
-  char *s;
-
-  if (str == NULL)
-    return (0);
-
-  l = 0;
-  while (*(str + l))
-    l++;
-
-  s = malloc(sizeof(char) * l + 1);
-
-  if (s == 0)
-    return (0);
-
-  for (i = 0; i <= l; i++)
-    {
-      *(s + i) = *(str + i);
-    }
-  return (s);
+int l, i;
+char *s;
+if (str == NULL)
+return (0);
+l = 0;
+while (*(str + l))
+l++;
+s = malloc(sizeof(char) * l + 1);
+if (s == 0)
+return (0);
+for (i = 0; i <= l; i++)
+{
+*(s + i) = *(str + i);
 }
-
+return (s);
+}
 /**
  *new_dog - new dog
  *@name: name of dog
@@ -42,35 +34,25 @@ char *_strdup(char *str)
  *
  *Return: dot_t type
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  dog_t *my_dog;
-
-  my_dog = malloc(sizeof(dog_t));
-  if (my_dog == NULL)
-    return (NULL);
-  my_dog->name = _strdup(name);
-  if (my_dog->name == 0)
-
-    {
-
-      free(my_dog);
-
-      return (0);
-
-    }
-  my_dog->age = age;
-  my_dog->owner = _strdup(owner);
-  if (my_dog->owner == 0)
-    {
-
-      free(my_dog);
-
-      free(my_dog->name);
-
-      return (0);
-
-    }
-  return (my_dog);
+dog_t *my_dog;
+my_dog = malloc(sizeof(dog_t));
+if (my_dog == NULL)
+return (NULL);
+my_dog->name = _strdup(name);
+if (my_dog->name == 0)
+{
+free(my_dog);
+return (0);
+}
+my_dog->age = age;
+my_dog->owner = _strdup(owner);
+if (my_dog->owner == 0)
+{
+free(my_dog);
+free(my_dog->name);
+return (0);
+}
+return (my_dog);
 }
