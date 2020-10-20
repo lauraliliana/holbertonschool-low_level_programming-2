a05c3afd1b3bc322b1b3f724b9052787b2e777ba
root <root@LAPTOP-DD7UF48T.localdomain>
2020-10-19 21:55:24 -0500

    Add exercises and .h w betty

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - defines a structure
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Structure dog has 3 members
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
