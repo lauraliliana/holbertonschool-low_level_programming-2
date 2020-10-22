f099a7df38e3d0ee2cc7852c19502d1ce1a6f329
root <root@LAPTOP-DD7UF48T.localdomain>
2020-10-21 23:34:56 -0500

    Update .h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,27 +1,3 @@
-#ifndef CALC
-#define CALC
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-typedef struct op
-{
-char *op;
-int (*f)(int a, int b);
-} op_t;
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-#endif
-
 #ifndef STRUCT_H
 #define STRUCT_H
 #include <stddef.h>
@@ -40,6 +16,9 @@ int (*f)(int a, int b);
 
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
