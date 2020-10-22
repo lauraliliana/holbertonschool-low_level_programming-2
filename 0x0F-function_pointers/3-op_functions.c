53e1059f921ed2f0296edccacf850549752fe0a8
root <root@LAPTOP-DD7UF48T.localdomain>
2020-10-21 23:42:36 -0500

    Changed op

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -39,8 +39,6 @@ int op_div(int a, int b)
 {
 if (b != 0)
 return (a / b);
-printf("Error\n");
-exit(100);
 }
 /**
  * op_mod - modulo of a / b
@@ -52,6 +50,4 @@ int op_mod(int a, int b)
 {
 if (b != 0)
 return (a % b);
-printf("Error\n");
-exit(100);
 }
